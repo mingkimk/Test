939f13aed0bfd3617a13e32774a5bfe03d139ad4
HU-203-06 <HU-203-06@HU-203-06-PC>
2020-03-17 12:41:33 +0900

    2020.03.17 ver1.0 (mk)

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,14 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int a=10;
-		int b=20;
-		
-		System.out.println(a+b);
+		int a = 10;
+		int b = 20;
 		
 		
-		
+//2020. 03.17 .17일  더하기 수정 박민경
+		int c = a + b;
+		System.out.println(c);
+
 	}
 
 }
